c68c3994094c50c20c33629d46c6e8d864180efb
Jeremy Brown <jebrown@softlayer.com>
2014-11-19 15:33:15 -0600

    Improvement to blueprint

--- blueprints/sl-test-helpers/index.js
+++ blueprints/sl-test-helpers/index.js
@@ -1,7 +1,18 @@
-var file         = 'tests/helpers/start-app.js',
-    textToInsert = "import sl-registerTestHelpers from '../../helpers/sl/register-test-helpers';" + EOL,
-    afterText    = "import Ember from 'ember';" + EOL;
+/* globals module */
 
 module.exports = {
-    insertIntoFile( file, textToInsert, { after: afterText } );
+    description: 'Register test helpers',
+
+    afterInstall: function( options ) {
+        var file,
+            textToInsert,
+            afterText;
+
+        // Import statement
+        file         = 'tests/helpers/start-app.js',
+        textToInsert = "import sl-registerTestHelpers from '../../helpers/sl/register-test-helpers';",
+        afterText    = "import Ember from 'ember';";
+
+        return this.insertIntoFile( file, textToInsert, { after: afterText } );
+    }
 };
