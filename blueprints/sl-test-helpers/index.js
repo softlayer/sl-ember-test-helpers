570d02f21fbe949df854c53198614795654d2edc
Jeremy Brown <jebrown@softlayer.com>
2014-11-20 09:03:52 -0600

    Desired implementation

--- blueprints/sl-test-helpers/index.js
+++ blueprints/sl-test-helpers/index.js
@@ -1,18 +1,39 @@
 /* globals module */
 
+var EOL = require('os').EOL;
+
 module.exports = {
     description: 'Register test helpers',
 
     afterInstall: function( options ) {
-        var file,
-            textToInsert,
-            afterText;
+            // Import statement
+        var firstFile          = 'tests/helpers/start-app.js',
+            firstText          = "import slregisterTestHelpers from './sl/register-test-helpers';",
+            firstLocationText  = "import Ember from 'ember';" + EOL,
 
-        // Import statement
-        file         = 'tests/helpers/start-app.js',
-        textToInsert = "import sl-registerTestHelpers from '../../helpers/sl/register-test-helpers';",
-        afterText    = "import Ember from 'ember';";
+            // Execution of registration function
+            secondFile         = 'tests/helpers/start-app.js',
+            secondText         = "    slregisterTestHelpers();",
+            secondLocationText = "App.setupForTesting();" + EOL,
 
-        return this.insertIntoFile( file, textToInsert, { after: afterText } );
-    }
+            // .jshintrc file
+            thirdFile          = 'tests/.jshintrc',
+            thirdText          = '    "contains",',
+            thirdLocationText  = '"predef": [' + EOL;
+
+            // Import statement
+        return this.insertIntoFile( firstFile, firstText, { after: firstLocationText } )
+
+            // Execution of registration function
+            .then( function() {
+                return this.insertIntoFile( secondFile, secondText, { after: secondLocationText } );
+            }.bind(this))
+
+            // .jshintrc file
+            .then( function() {
+                return this.insertIntoFile( thirdFile, thirdText, { after: thirdLocationText } );
+            }.bind(this));
+    },
+
+    normalizeEntityName: function() {}
 };
