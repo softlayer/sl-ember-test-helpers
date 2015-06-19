c833d80bf77958528ed0c80dd5671bf63d012b8f
Jeremy Brown <jebrown@softlayer.com>
2015-06-19 09:50:36 -0500

    Update naming styles

--- blueprints/sl-ember-test-helpers/index.js
+++ blueprints/sl-ember-test-helpers/index.js
@@ -8,12 +8,12 @@ module.exports = {
     afterInstall: function( options ) {
         // Import statement
         var firstFile = 'tests/helpers/start-app.js';
-        var firstText = "import slregisterTestHelpers from './sl/register-test-helpers';";
+        var firstText = "import slRegisterTestHelpers from './sl/register-test-helpers';";
         var firstLocationText = "import Ember from 'ember';" + EOL;
 
         // Execution of registration function
         var secondFile = 'tests/helpers/start-app.js';
-        var secondText = "    slregisterTestHelpers();";
+        var secondText = "    slRegisterTestHelpers();";
         var secondLocationText = "application.setupForTesting();" + EOL;
 
         // .jshintrc file
--- test-support/helpers/sl/register-test-helpers.js
+++ test-support/helpers/sl/register-test-helpers.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 import {
     contains,
-    ajax,
+    AjaxHelper,
     requires
 } from './synchronous';
 
 export default function() {
     Ember.Test.registerHelper( 'contains', contains );
-    Ember.Test.registerHelper( 'ajax', ajax );
+    Ember.Test.registerHelper( 'Ajax', AjaxHelper );
     Ember.Test.registerHelper( 'requires', requires );
 }
