d2841ed704471f1586ca613a762e916addeb5fe1
Jeremy Brown <jebrown@softlayer.com>
2015-08-03 13:50:10 -0500

    Closes softlayer/sl-ember-test-helpers#107

---
+++ ember-cli-build.js
@@ -0,0 +1,48 @@
+/* global require, module */
+const EmberApp = require( 'ember-cli/lib/broccoli/ember-addon' );
+const packageConfig = require( './package.json' );
+const replace = require( 'broccoli-string-replace' );
+
+module.exports = function( defaults ) {
+    const app = new EmberApp( defaults, {
+        // Add options here
+    });
+
+    /*
+    This build file specifes the options for the dummy test app of this
+    addon, located in `/tests/dummy`
+    This build file does *not* influence how the addon or the app using it
+    behave. You most likely want to be modifying `./index.js` or app's build file
+    */
+
+    // Testing dependencies
+    app.import( app.bowerDirectory + '/sinonjs/sinon.js', {
+        type: 'test'
+    });
+
+    app.import( app.bowerDirectory + '/sinon-qunit/lib/sinon-qunit.js', {
+        type: 'test'
+    });
+
+    const tree = replace( app.toTree(), {
+        files: [
+            'index.html',
+            'assets/dummy.js'
+        ],
+        patterns: [
+            {
+                match: /REPLACE_META_DESCRIPTION/g,
+                replacement: packageConfig.description
+            }, {
+                match: /REPLACE_META_KEYWORDS/g,
+                replacement: packageConfig.keywords.join( ', ' ) +
+                    ', ember, ember cli'
+            }, {
+                match: /REPLACE_APPLICATION_VERSION/g,
+                replacement: packageConfig.version
+             }
+        ]
+    });
+
+    return tree;
+};
