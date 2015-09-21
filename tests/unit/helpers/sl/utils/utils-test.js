0f995d2a0cdce816e3d072c98dc28a40b54c308f
Jeremy Brown <jebrown@softlayer.com>
2015-09-21 14:28:41 -0500

    Resolve linting issues introdued during use of "let"

--- tests/unit/helpers/sl/utils/utils-test.js
+++ tests/unit/helpers/sl/utils/utils-test.js
@@ -212,20 +212,20 @@ test( 'convertStringToArray() requires a string to be provided', function( asser
     );
 });
 
-let testName = 'convertStringToArray() returns an array with a single' +
+const testName1 = 'convertStringToArray() returns an array with a single' +
 'element when passed a string without spaces';
 
-test( testName, function( assert ) {
+test( testName1, function( assert ) {
     assert.deepEqual(
         convertStringToArray( 'testString' ),
         [ 'testString' ]
     );
 });
 
-let testName = 'convertStringToArray() returns an array with as many elements as there' +
+const testName2 = 'convertStringToArray() returns an array with as many elements as there' +
 'are "words" represented in a string passed to it containing spaces';
 
-test( testName, function( assert ) {
+test( testName2, function( assert ) {
     assert.deepEqual(
         convertStringToArray( 'the test string' ),
         [ 'the', 'test', 'string' ]
