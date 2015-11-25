1feb16e4edcfb1a10edfe87da29441e35b6fb0d8
Aziz Punjani <aziz.punjani@gmail.com>
2015-11-25 13:30:08 -0600

    Changed API interface a little to accomodate for event firing + added some more tests

--- tests/unit/helpers/sl/synchronous/global-libaries-test.js
+++ tests/unit/helpers/sl/synchronous/global-libaries-test.js
@@ -1,5 +1,7 @@
+import Ember from 'ember';
 import { test } from 'ember-qunit';
 import * as globalLibraries from '../../../../helpers/sl/synchronous/global-libraries';
+import sinon from 'sinon';
 
 module( 'Unit | Helpers | sl/synchronous/global-libraries' );
 
@@ -10,14 +12,81 @@ test( 'it exists', function( assert ) {
     );
 });
 
-test( 'Functions as expected', function( assert ) {
+test( 'Passes when global libraries are not referenced', function( assert ) {
+    const component = Ember.Object.extend().create({
+        trigger: function() {}
+    });
+
     globalLibraries.setupSpies();
 
-    globalLibraries.restoreSpies();
+    globalLibraries.triggerEvents( component );
 
-    assert.equal(
-        globalLibraries.called,
-        false,
-        'There are no references to Ember.$, $ or jQuery'
+    assert.notOk(
+        globalLibraries.called()
     );
+
+    globalLibraries.restoreSpies();
+});
+
+test( 'triggerEvents triggers respective events', function( assert ) {
+    const events = {
+        'willInsertElement': sinon.spy() ,
+        'didInsertElement': sinon.spy() ,
+        'willClearRender': sinon.spy() ,
+        'willDestroyElement': sinon.spy()
+    };
+
+    const component = {
+        trigger: function( eventName ) {
+            this.events[ eventName ]();
+        },
+
+        events
+    };
+
+    globalLibraries.triggerEvents( component );
+
+    Object.keys( events ).forEach(( eventName ) => {
+        const spy = events[ eventName ];
+        assert.ok(
+            spy.called,
+            `${ eventName } was triggered`
+        );
+    });
+});
+
+test( 'called() returns true when $ is referenced', function( assert ) {
+    globalLibraries.setupSpies();
+
+    window.$();
+
+    assert.ok(
+        globalLibraries.called()
+    );
+
+    globalLibraries.restoreSpies();
+});
+
+test( 'called() returns true when jQuery is referenced', function( assert ) {
+    globalLibraries.setupSpies();
+
+    window.jQuery();
+
+    assert.ok(
+        globalLibraries.called()
+    );
+
+    globalLibraries.restoreSpies();
+});
+
+test( 'called() returns true when Ember.$ is referenced', function( assert ) {
+    globalLibraries.setupSpies();
+
+    Ember.$();
+
+    assert.ok(
+        globalLibraries.called()
+    );
+
+    globalLibraries.restoreSpies();
 });
